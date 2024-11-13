b84d2ea2bc19b504d60bdbaac3a2c164f91a6237
Niko Gorjan <niko.gorjan@gmail.com>
2024-11-13 22:38:51 +0100

    made the star snimation button background transparent

--- app/_components/Hero/components/StarButton.tsx
+++ app/_components/Hero/components/StarButton.tsx
@@ -31,152 +31,154 @@ const StarButton: React.FC<StarButtonProps> = ({ text }) => {
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <Button className="custom-button-font inner-button font-bold w-full sm:w-auto">
-        <div className="flex items-center gap-[6px] w-full sm:w-auto justify-center relative">
-          {/* Display dynamic text passed as a prop */}
-          <div className="mr-2">{text}</div>
+      <div className="gradient-border-bg">
+        <Button className="custom-button-font inner-button font-bold w-full sm:w-auto border-none">
+          <div className="flex items-center gap-[6px] w-full sm:w-auto justify-center relative">
+            {/* Display dynamic text passed as a prop */}
+            <div className="mr-2">{text}</div>
 
-          {/* Expandable div with star animation */}
-          <motion.div
-            id="expandable"
-            className="h-full flex items-center overflow-visible"
-            initial={{ width: 0 }}
-            animate={{ width: isExpandableOpen ? 20 : 0 }}
-            transition={{
-              duration: 0.4,
-              ease: "easeInOut",
-            }}
-          >
-            <AnimatePresence>
-              {isExpandableOpen && (
-                <>
-                  {/* Star animations */}
-                  <motion.img
-                    src="/Icons/star-small.svg"
-                    alt="Star Small"
-                    className="absolute"
-                    initial={{ opacity: 0, x: 0, y: 0 }}
-                    animate={{
-                      opacity: [0, 1, 0],
-                      x: -10,
-                      y: -20,
-                    }}
-                    transition={{
-                      duration: 0.8,
-                      ease: "easeInOut",
-                    }}
-                  />
-                  <motion.img
-                    src="/Icons/star-mid.svg"
-                    alt="Star Mid"
-                    className="absolute"
-                    initial={{ opacity: 0, x: 0, y: 0 }}
-                    animate={{
-                      opacity: [0, 1, 0],
-                      x: -15,
-                      y: -30,
-                    }}
-                    transition={{
-                      delay: 0.2,
-                      duration: 0.8,
-                      ease: "easeInOut",
-                    }}
-                  />
-                  <motion.img
-                    src="/Icons/star-big.svg"
-                    alt="Star Big"
-                    className="absolute"
-                    initial={{ opacity: 0, x: 0, y: 0 }}
-                    animate={{
-                      opacity: [0, 1, 0],
-                      x: -20,
-                      y: -40,
-                    }}
-                    transition={{
-                      delay: 0.4,
-                      duration: 0.8,
-                      ease: "easeInOut",
-                    }}
-                  />
-                  <motion.img
-                    src="/Icons/star-big.svg"
-                    alt="Star Big 2"
-                    className="absolute"
-                    initial={{ opacity: 0, x: 0, y: 0 }}
-                    animate={{
-                      opacity: [0, 1, 0],
-                      x: 14,
-                      y: -40,
-                    }}
-                    transition={{
-                      delay: 0.6,
-                      duration: 0.8,
-                      ease: "easeInOut",
-                    }}
-                  />
-                  <motion.img
-                    src="/Icons/star-small.svg"
-                    alt="Star Small 2"
-                    className="absolute"
-                    initial={{ opacity: 0, x: 0, y: 0 }}
-                    animate={{
-                      opacity: [0, 1, 0],
-                      x: 6,
-                      y: -40,
-                    }}
-                    transition={{
-                      delay: 0.8,
-                      duration: 0.8,
-                      ease: "easeInOut",
-                    }}
-                  />
-                  <motion.img
-                    src="/Icons/star-mid.svg"
-                    alt="Star Mid 2"
-                    className="absolute"
-                    initial={{ opacity: 0, x: 0, y: 0 }}
-                    animate={{
-                      opacity: [0, 1, 0],
-                      x: 9,
-                      y: -40,
-                    }}
-                    transition={{
-                      delay: 1,
-                      duration: 0.8,
-                      ease: "easeInOut",
-                    }}
-                  />
-                  <motion.img
-                    src="/Icons/star-mid.svg"
-                    alt="Star Mid 3"
-                    className="absolute"
-                    initial={{ opacity: 0, x: 0, y: 0 }}
-                    animate={{
-                      opacity: [0, 1, 0],
-                      x: 5,
-                      y: -40,
-                    }}
-                    transition={{
-                      delay: 1.2,
-                      duration: 0.8,
-                      ease: "easeInOut",
-                    }}
-                  />
-                </>
-              )}
-            </AnimatePresence>
-          </motion.div>
+            {/* Expandable div with star animation */}
+            <motion.div
+              id="expandable"
+              className="h-full flex items-center overflow-visible"
+              initial={{ width: 0 }}
+              animate={{ width: isExpandableOpen ? 20 : 0 }}
+              transition={{
+                duration: 0.4,
+                ease: "easeInOut",
+              }}
+            >
+              <AnimatePresence>
+                {isExpandableOpen && (
+                  <>
+                    {/* Star animations */}
+                    <motion.img
+                      src="/Icons/star-small.svg"
+                      alt="Star Small"
+                      className="absolute"
+                      initial={{ opacity: 0, x: 0, y: 0 }}
+                      animate={{
+                        opacity: [0, 1, 0],
+                        x: -10,
+                        y: -20,
+                      }}
+                      transition={{
+                        duration: 0.8,
+                        ease: "easeInOut",
+                      }}
+                    />
+                    <motion.img
+                      src="/Icons/star-mid.svg"
+                      alt="Star Mid"
+                      className="absolute"
+                      initial={{ opacity: 0, x: 0, y: 0 }}
+                      animate={{
+                        opacity: [0, 1, 0],
+                        x: -15,
+                        y: -30,
+                      }}
+                      transition={{
+                        delay: 0.2,
+                        duration: 0.8,
+                        ease: "easeInOut",
+                      }}
+                    />
+                    <motion.img
+                      src="/Icons/star-big.svg"
+                      alt="Star Big"
+                      className="absolute"
+                      initial={{ opacity: 0, x: 0, y: 0 }}
+                      animate={{
+                        opacity: [0, 1, 0],
+                        x: -20,
+                        y: -40,
+                      }}
+                      transition={{
+                        delay: 0.4,
+                        duration: 0.8,
+                        ease: "easeInOut",
+                      }}
+                    />
+                    <motion.img
+                      src="/Icons/star-big.svg"
+                      alt="Star Big 2"
+                      className="absolute"
+                      initial={{ opacity: 0, x: 0, y: 0 }}
+                      animate={{
+                        opacity: [0, 1, 0],
+                        x: 14,
+                        y: -40,
+                      }}
+                      transition={{
+                        delay: 0.6,
+                        duration: 0.8,
+                        ease: "easeInOut",
+                      }}
+                    />
+                    <motion.img
+                      src="/Icons/star-small.svg"
+                      alt="Star Small 2"
+                      className="absolute"
+                      initial={{ opacity: 0, x: 0, y: 0 }}
+                      animate={{
+                        opacity: [0, 1, 0],
+                        x: 6,
+                        y: -40,
+                      }}
+                      transition={{
+                        delay: 0.8,
+                        duration: 0.8,
+                        ease: "easeInOut",
+                      }}
+                    />
+                    <motion.img
+                      src="/Icons/star-mid.svg"
+                      alt="Star Mid 2"
+                      className="absolute"
+                      initial={{ opacity: 0, x: 0, y: 0 }}
+                      animate={{
+                        opacity: [0, 1, 0],
+                        x: 9,
+                        y: -40,
+                      }}
+                      transition={{
+                        delay: 1,
+                        duration: 0.8,
+                        ease: "easeInOut",
+                      }}
+                    />
+                    <motion.img
+                      src="/Icons/star-mid.svg"
+                      alt="Star Mid 3"
+                      className="absolute"
+                      initial={{ opacity: 0, x: 0, y: 0 }}
+                      animate={{
+                        opacity: [0, 1, 0],
+                        x: 5,
+                        y: -40,
+                      }}
+                      transition={{
+                        delay: 1.2,
+                        duration: 0.8,
+                        ease: "easeInOut",
+                      }}
+                    />
+                  </>
+                )}
+              </AnimatePresence>
+            </motion.div>
 
-          {/* Arrow Icon */}
-          <div className="w-4 h-4">
-            <img
-              src="/Icons/arrow-rigth.svg"
-              alt="Arrow Right Icon"
-              className="w-full h-full"
-            />
+            {/* Arrow Icon */}
+            <div className="w-4 h-4">
+              <img
+                src="/Icons/arrow-rigth.svg"
+                alt="Arrow Right Icon"
+                className="w-full h-full"
+              />
+            </div>
           </div>
-        </div>
-      </Button>
+        </Button>
+      </div>
     </div>
   );
 };
