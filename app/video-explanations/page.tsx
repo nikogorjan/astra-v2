affa049d39585da39de28f3d056894d01092c1a6
Niko Gorjan <niko.gorjan@gmail.com>
2024-11-18 18:20:39 +0100

    video explanations

---
+++ app/video-explanations/page.tsx
@@ -0,0 +1,14 @@
+import { Footer } from "@/components/Footer/Footer";
+import { Lines } from "@/components/Lines/Lines";
+import { Navbar5 } from "@/components/Navbar/Navbar5";
+
+export default function Home() {
+  return (
+    <main>
+      <Lines />
+      <Navbar5 />
+
+      <Footer />
+    </main>
+  );
+}
