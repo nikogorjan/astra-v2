8e25d439ffb9173753183b36978476e7bca5db4a
Niko Gorjan <niko.gorjan@gmail.com>
2024-11-26 22:18:38 +0100

    commit

--- app/resources/_components/Header/components/AnimatedHeader/AnimatedHeading.tsx
+++ app/resources/_components/Header/components/AnimatedHeader/AnimatedHeading.tsx
@@ -1,29 +1,6 @@
 import { motion } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
 
 export const AnimatedHeading = () => {
-  const [isCombined, setIsCombined] = useState(false);
-  const availableRef = useRef<HTMLSpanElement>(null);
-  const twentyFourSevenRef = useRef<HTMLSpanElement>(null);
-
-  useEffect(() => {
-    const checkIfCombined = () => {
-      if (availableRef.current && twentyFourSevenRef.current) {
-        const availableRect = availableRef.current.getBoundingClientRect();
-        const twentyFourSevenRect =
-          twentyFourSevenRef.current.getBoundingClientRect();
-
-        // Check if they are on the same line
-        setIsCombined(availableRect.top === twentyFourSevenRect.top);
-      }
-    };
-
-    checkIfCombined();
-    window.addEventListener("resize", checkIfCombined);
-
-    return () => window.removeEventListener("resize", checkIfCombined);
-  }, []);
-
   return (
     <h1 className="mb-5 text-6xl md:text-[4rem] font-bold leading-10 md:leading-[64px]">
       Study{" "}
